d2fbf1d8799da573754a8e4ce1e6128cf5266b2c
Hakiki Tech <57021097+hakikicode@users.noreply.github.com>
2024-11-06 19:25:49 +0100

    Update 2-submission.js

--- src/task/2-submission.js
+++ src/task/2-submission.js
@@ -1,14 +1,11 @@
 import { namespaceWrapper } from "@_koii/namespace-wrapper";
 
 export async function submission(roundNumber) {
-  /**
-   * Submit the task proofs for auditing
-   * Must return a string of max 512 bytes to be submitted on chain
-   */
   try {
-    console.log(`MAKE SUBMISSION FOR ROUND ${roundNumber}`);
-    return await namespaceWrapper.storeGet("value");
+    console.log(`Submitting data for round ${roundNumber}`);
+    const gameData = await namespaceWrapper.storeGet(`round_${roundNumber}_gameData`);
+    return gameData;
   } catch (error) {
-    console.error("MAKE SUBMISSION ERROR:", error);
+    console.error("Submission error:", error);
   }
 }
